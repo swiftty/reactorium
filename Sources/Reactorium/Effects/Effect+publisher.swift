0f46ae11d1571335de6a1986debc7ef533e99b8f
swiftty <62803132+swiftty@users.noreply.github.com>
2022-11-12 00:29:34 +0900

    add TestStore

--- Sources/Reactorium/Effects/Effect+publisher.swift
+++ Sources/Reactorium/Effects/Effect+publisher.swift
@@ -1,5 +1,5 @@
 import Foundation
-import Combine
+@preconcurrency import Combine
 
 extension Effect where Action: Sendable {
     @inlinable
@@ -13,5 +13,30 @@ extension Effect where Action: Sendable {
                 send(action)
             }
         })
+    }
+}
+
+extension Effect: Publisher {
+    public typealias Output = Action
+    public typealias Failure = Never
+
+    public func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
+        switch operation {
+        case .none:
+            Empty().subscribe(subscriber)
+
+        case .task(let priority, let body):
+            let subject = PassthroughSubject<Action, Never>()
+            let task = Task(priority: priority) { @MainActor in
+                defer { subject.send(completion: .finished) }
+                await body(Send { subject.send($0) })
+            }
+
+            subject
+                .handleEvents(receiveCancel: {
+                    task.cancel()
+                })
+                .subscribe(subscriber)
+        }
     }
 }
