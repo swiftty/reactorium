4f1317a76aa86d38ff8c437fb432ee039e1fdaaf
swiftty <62803132+swiftty@users.noreply.github.com>
2022-11-13 16:32:54 +0900

    support macOS, tvOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "reactorium",
     platforms: [
-        .iOS(.v14)
+        .macOS(.v11), .iOS(.v14), .tvOS(.v14)
     ],
     products: [
         .library(
