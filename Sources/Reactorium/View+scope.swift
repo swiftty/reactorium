40a025a5d2655403c0ec36af800458bd6216667b
swiftty <62803132+swiftty@users.noreply.github.com>
2022-11-16 09:04:10 +0900

    add `key: Equatable` to scope methods

--- Sources/Reactorium/View+scope.swift
+++ Sources/Reactorium/View+scope.swift
@@ -4,57 +4,65 @@ import SwiftUI
 extension View {
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable, Action, Dependency
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Dependency>,
         dependency: @escaping (EnvironmentValues) -> Dependency
     ) -> some View {
-        modifier(ProxyModifier(dependency: dependency) { dependency in
+        modifier(ProxyModifier(key: key, dependency: dependency) { dependency in
             Store(binding: binder, action: action, reducer: reducer, dependency: dependency)
         })
     }
 
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable, Action
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Void>
     ) -> some View {
-        scope(binding: binder, action: action, reducer: reducer, dependency: { _ in })
+        scope(key: key, binding: binder, action: action, reducer: reducer, dependency: { _ in })
     }
 
     // MARK: with equatable
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable & Equatable, Action, Dependency
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Dependency>,
         dependency: @escaping (EnvironmentValues) -> Dependency
     ) -> some View {
-        modifier(ProxyModifier(dependency: dependency) { dependency in
+        modifier(ProxyModifier(key: key, dependency: dependency) { dependency in
             Store(binding: binder, action: action, reducer: reducer, dependency: dependency, removeDuplicates: ==)
         })
     }
 
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable & Equatable, Action
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Void>
     ) -> some View {
-        scope(binding: binder, action: action, reducer: reducer, dependency: { _ in })
+        scope(key: key, binding: binder, action: action, reducer: reducer, dependency: { _ in })
     }
 }
 
@@ -63,10 +71,12 @@ extension View {
     @MainActor
     @ViewBuilder
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable, Action, Dependency,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Dependency>,
@@ -74,7 +84,7 @@ extension View {
         @ViewBuilder else elseContent: () -> ElseContent = { EmptyView() }
     ) -> some View {
         if let value = binder.value {
-            scope(binding: binder.map { $0 ?? value }, action: action, reducer: reducer, dependency: dependency)
+            scope(key: key, binding: binder.map { $0 ?? value }, action: action, reducer: reducer, dependency: dependency)
         } else {
             elseContent()
         }
@@ -82,26 +92,30 @@ extension View {
 
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable, Action,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Void>,
         @ViewBuilder else elseContent: () -> ElseContent = { EmptyView() }
     ) -> some View {
-        scope(binding: binder, action: action, reducer: reducer, dependency: { _ in }, else: elseContent)
+        scope(key: key, binding: binder, action: action, reducer: reducer, dependency: { _ in }, else: elseContent)
     }
 
     // MARK: with equatable
     @MainActor
     @ViewBuilder
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable & Equatable, Action, Dependency,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Dependency>,
@@ -109,7 +123,7 @@ extension View {
         @ViewBuilder else elseContent: () -> ElseContent = { EmptyView() }
     ) -> some View {
         if let value = binder.value {
-            scope(binding: binder.map { $0 ?? value }, action: action, reducer: reducer, dependency: dependency)
+            scope(key: key, binding: binder.map { $0 ?? value }, action: action, reducer: reducer, dependency: dependency)
         } else {
             elseContent()
         }
@@ -117,16 +131,18 @@ extension View {
 
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable & Equatable, Action,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Void>,
         @ViewBuilder else elseContent: () -> ElseContent = { EmptyView() }
     ) -> some View {
-        scope(binding: binder, action: action, reducer: reducer, dependency: { _ in }, else: elseContent)
+        scope(key: key, binding: binder, action: action, reducer: reducer, dependency: { _ in }, else: elseContent)
     }
 }
 
@@ -135,11 +151,13 @@ extension View {
 extension Layout {
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable, Action, Dependency,
         ThenContent: View,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Dependency>,
@@ -148,6 +166,7 @@ extension Layout {
         @ViewBuilder else elseContent: @escaping () -> ElseContent = { EmptyView() }
     ) -> some View {
         OptionalScoped(
+            key: key,
             binding: binder,
             action: action,
             reducer: reducer,
@@ -161,28 +180,32 @@ extension Layout {
 
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Sendable, Action,
         ThenContent: View,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Void>,
         @ViewBuilder then thenContent: @escaping () -> ThenContent,
         @ViewBuilder else elseContent: @escaping () -> ElseContent = { EmptyView() }
     ) -> some View {
-        scope(binding: binder, action: action, reducer: reducer, dependency: { _ in }, then: thenContent, else: elseContent)
+        scope(key: key, binding: binder, action: action, reducer: reducer, dependency: { _ in }, then: thenContent, else: elseContent)
     }
 
     // MARK: with equatable
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Equatable & Sendable, Action, Dependency,
         ThenContent: View,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Dependency>,
@@ -191,6 +214,7 @@ extension Layout {
         @ViewBuilder else elseContent: @escaping () -> ElseContent = { EmptyView() }
     ) -> some View {
         OptionalScoped(
+            key: key,
             binding: binder,
             action: action,
             reducer: reducer,
@@ -203,30 +227,33 @@ extension Layout {
 
     @MainActor
     public func scope<
+        Key: Equatable,
         PState: Sendable, PAction, PDependency,
         State: Equatable & Sendable, Action,
         ThenContent: View,
         ElseContent: View
     >(
+        key: Key? = 0,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<State?>,
         action: @escaping (State) -> PAction,
         reducer: some Reducer<State, Action, Void>,
         @ViewBuilder then thenContent: @escaping () -> ThenContent,
         @ViewBuilder else elseContent: @escaping () -> ElseContent = { EmptyView() }
     ) -> some View {
-        scope(binding: binder, action: action, reducer: reducer, dependency: { _ in }, then: thenContent, else: elseContent)
+        scope(key: key, binding: binder, action: action, reducer: reducer, dependency: { _ in }, then: thenContent, else: elseContent)
     }
 }
 
 // MARK: -
-private struct ProxyModifier<State: Sendable, Action, Dependency>: ViewModifier {
+private struct ProxyModifier<Key: Equatable, State: Sendable, Action, Dependency>: ViewModifier {
     @StateObject var proxy = ProxyStore<State, Action, Dependency>()
+    let key: Key?
     let dependency: (EnvironmentValues) -> Dependency
     let inject: (Dependency) -> Store<State, Action, Dependency>
 
     func body(content: Content) -> some View {
         ZStack {
-            DismantleView(proxy: proxy, dependency: dependency, inject: inject)
+            DismantleView(proxy: proxy, key: key, dependency: dependency, inject: inject)
                 .opacity(0)
                 .frame(width: 0, height: 0)
 
@@ -240,12 +267,14 @@ private struct ProxyModifier<State: Sendable, Action, Dependency>: ViewModifier 
 
 @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
 private struct OptionalScoped<
+    Key: Equatable,
     PState: Sendable, PAction, PDependency,
     R: Reducer,
     Layout: SwiftUI.Layout,
     ThenContent: View,
     ElseContent: View
 >: View where R.State: Sendable {
+    let key: Key?
     let binder: Store<PState, PAction, PDependency>.Bindable.Binder<R.State?>
     let action: (R.State) -> PAction
     let reducer: R
@@ -258,6 +287,7 @@ private struct OptionalScoped<
     @StateObject var proxy = ProxyStore<R.State, R.Action, R.Dependency>()
 
     init(
+        key: Key?,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<R.State?>,
         action: @escaping (R.State) -> PAction,
         reducer: R,
@@ -267,6 +297,7 @@ private struct OptionalScoped<
         @ViewBuilder else elseContent: @escaping () -> ElseContent = { EmptyView() },
         removeDuplicates isDuplicates: ((R.State, R.State) -> Bool)?
     ) {
+        self.key = key
         self.binder = binder
         self.action = action
         self.reducer = reducer
@@ -278,6 +309,7 @@ private struct OptionalScoped<
     }
 
     init(
+        key: Key?,
         binding binder: Store<PState, PAction, PDependency>.Bindable.Binder<R.State?>,
         action: @escaping (R.State) -> PAction,
         reducer: R,
@@ -286,14 +318,14 @@ private struct OptionalScoped<
         @ViewBuilder then thenContent: @escaping () -> ThenContent,
         @ViewBuilder else elseContent: @escaping () -> ElseContent = { EmptyView() }
     ) where R.State: Equatable {
-        self.init(binding: binder, action: action, reducer: reducer, dependency: dependency,
+        self.init(key: key, binding: binder, action: action, reducer: reducer, dependency: dependency,
                   in: layout, then: thenContent, else: elseContent, removeDuplicates: ==)
     }
 
     var body: some View {
         ZStack {
             if let state = binder.value {
-                DismantleView(proxy: proxy, dependency: dependency, inject: { dependency in
+                DismantleView(proxy: proxy, key: key, dependency: dependency, inject: { dependency in
                     Store(
                         binding: binder.map { $0 ?? state },
                         action: action,
@@ -378,24 +410,32 @@ extension SystemViewRepresentable {
 #endif
 
 @MainActor
-private struct DismantleView<State: Sendable, Action, Dependency>: SystemViewRepresentable {
+private struct DismantleView<Key: Equatable, State: Sendable, Action, Dependency>: SystemViewRepresentable {
     @ObservedObject var proxy: ProxyStore<State, Action, Dependency>
+    let key: Key?
     let dependency: (EnvironmentValues) -> Dependency
     let inject: (Dependency) -> Store<State, Action, Dependency>
 
     class View: SystemView {
         var proxy: ProxyStore<State, Action, Dependency>?
+        var key: Key?
     }
 
     func makeView(context: Context) -> View {
         let view = View()
         proxy.store = inject(dependency(context.environment))
         view.proxy = proxy
+        view.key = key
         return view
     }
 
     func updateView(_ view: View, context: Context) {
-        proxy.store?.dependency = dependency(context.environment)
+        let dependency = dependency(context.environment)
+        if view.key != key {
+            view.key = key
+            proxy.store = inject(dependency)
+        }
+        proxy.store?.dependency = dependency
     }
 
     static func dismantleView(_ view: View, coordinator: ()) {
